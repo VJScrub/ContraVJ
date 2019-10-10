79bb6baf43b9a961b2f63f90219173fc69337341
Cros07 <edujseoane@gmail.com>
2019-10-10 19:07:14 +0200

    Primer commit

---
+++ 02-Bubble/Mapes/Contra-Stage1-Jungle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Contra-Stage1-Jungle" tilewidth="32" tileheight="32" tilecount="840" columns="120">
+ <image source="../images/Contra-Stage1-Jungle.png" width="3840" height="240"/>
+</tileset>
---
+++ 02-Bubble/Mapes/MapaTiles2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="MapaTiles2" tilewidth="16" tileheight="16" tilecount="390" columns="26">
+ <image source="../images/MapaTiles.png" width="416" height="240"/>
+</tileset>
