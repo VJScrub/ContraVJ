511dcdea636e12fd3052607a8889f86a0b0170a8
judit gonzalez gutierrez <judit.gonzalez.gutierrez@est.fib.upc.edu>
2019-10-25 00:55:00 +0200

    Enemies are niggers who hates niggas and no more bubbles

--- 02-Bubble/Enemy.cpp
+++ 02-Bubble/Enemy.cpp
@@ -18,29 +18,18 @@ enum EnemyAnims
 void Enemy::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 {
 	bJumping = false;
-	spritesheet.loadFromFile("images/bub2.png", TEXTURE_PIXEL_FORMAT_RGBA);
+	spritesheet.loadFromFile("images/runenemies.png", TEXTURE_PIXEL_FORMAT_RGBA);
     // crear Sprite(quadSize, sizeInSpritesheet, spritesheet, program);
-	sprite = Sprite::createSprite(glm::ivec2(32, 32), glm::vec2(0.25, 0.25), &spritesheet, &shaderProgram);
+	sprite = Sprite::createSprite(glm::ivec2(16, 32), glm::vec2(0.25, 1), &spritesheet, &shaderProgram);
 	sprite->setNumberAnimations(5);
 
 	sprite->setAnimationSpeed(STAND_LEFT, 8);
 	sprite->addKeyframe(STAND_LEFT, glm::vec2(0.f, 0.f));
 
-	sprite->setAnimationSpeed(STAND_RIGHT, 8);
-	sprite->addKeyframe(STAND_RIGHT, glm::vec2(0.25f, 0.f));
-
 	sprite->setAnimationSpeed(MOVE_LEFT, 8);
 	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.f));
-	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.25f));
-	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.5f));
-
-	sprite->setAnimationSpeed(MOVE_RIGHT, 8);
-	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.f));
-	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.25f));
-	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.5f));
-
-	sprite->setAnimationSpeed(DOWN_RIGHT, 8);
-	sprite->addKeyframe(DOWN_RIGHT, glm::vec2(0.f, 0.75f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.25f, 0.f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.5f, 0.f));
 
 	sprite->changeAnimation(0);
 	tileMapDispl = tileMapPos;
